34d655dc2b5811f6e7f488c7022fe8323772a647
alexMg <cigurd186@icloud.com>
2026-02-08 18:48:31 +0700

    add system prompt

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import feedparser
-
+from pathlib import Path
+from langchain_core.prompts.loading import load_prompt
 urls = "https://aws.amazon.com/blogs/training-and-certification/feed"
+
+
+prompt_config = Path("system_prompt.yaml")
+prompt = load_prompt(prompt_config)
 
 
 def rss_feed(url: str):
     feed = feedparser.parse(url)
-    print(feed['entries'][:5])
+    print(feed['entries'][:10])
 
 
 if __name__ == '__main__':
